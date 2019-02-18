06b77604f1770ae9a9069e22440764fe655b21cd
Alex Garcia and Ben Kirby <example@example.com>
2019-02-18 15:55:43 -0800

    All day

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,47 +1,112 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Icon from '@mdi/react'
+import { mdiComment, mdiTwitterRetweet, mdiHeart } from '@mdi/js'
 
-function Tweet (props){
-  const tweetBoxStyles = {
+
+const styles = {
+  tweetBox: {
     border: 'none',
     backgroundColor: 'white',
     margin: '10px 10px 0px 10px',
     padding: '5px',
     fontFamily: 'Arial'
-  };
-
-  const imgStyle = {
+  },
+  image: {
     height: '40px',
     width: '40px',
     border: '3px solid lightgrey',
     borderRadius: '25rem',
-  }
-
-  const profilePicStyle = {
+  },
+  profilePic: {
     height: '30px',
     width: '30px',
     margin: '5px 0px 0px 5px'
-  };
-
-  const tweetContentStyle = {
+  },
+  tweetContent: {
     margin: '-45px 5px 0px 60px',
     paddingBottom: '3px'
+  },
+  flex: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: '15px'
+  },
+  icon: {
+    height: '25px',
+    width: '25px',
+    marginRight: '5px'
+  },
+  iconRow: {
+    display: 'flex',
+    alignItems: 'center',
+    marginRight: '5px'
+  }
+}
+
+class Tweet extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state={
+      isLiked:false,
+      likeCount: props.likes
+    };
+   this.handleClick = this.handleClick.bind(this);
   }
 
-  return (
-    <div>
-      <div style={tweetBoxStyles}>
-        <div style={profilePicStyle}>
-          <img style={imgStyle} src={`${props.profilePic}`}></img>
-        </div>
-        <div style={tweetContentStyle}>
-        <p><strong>{props.name}</strong> @{props.username}</p>
-        <p>{props.body}</p>
+  handleClick(){
+    let likeCount = this.props.likes;
+    console.log(likeCount);
+    this.setState({isLiked: !this.state.isLiked});
+    console.log(this.state.isLiked);
+
+    if (this.state.isLiked === true) {
+      likeCount+=1
+      // return likeCount
+    }
+    else if (this.state.isLIked === false) {
+      likeCount-=1
+      // return likeCount
+    }
+    this.setState({likeCount: likeCount});
+  }
+
+
+render(){
+
+
+    return (
+      <div>
+        <div style={styles.tweetBox}>
+          <div style={styles.profilePic}>
+            <img style={styles.image} src={`${this.props.profilePic}`}></img>
+          </div>
+          <div style={styles.tweetContent}>
+          <p><strong>{this.props.name}</strong> @{this.props.username}</p>
+          <p>{this.props.body}</p>
+          </div>
+
+          <div style={styles.flex}>
+            <div style={styles.iconRow}>
+              <Icon style={styles.icon} path={mdiComment}/>
+              <p>{this.props.comments}</p>
+            </div>
+
+            <div style={styles.iconRow}>
+              <Icon style={styles.icon} path={mdiTwitterRetweet}/>
+            </div>
+
+            <div style={styles.iconRow}>
+              <Icon onClick={this.handleClick} style={styles.icon} path={mdiHeart}/>
+                <p>{this.state.likeCount}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Tweet.propTypes = {
--- src/components/TweetList.jsx
+++ src/components/TweetList.jsx
@@ -1,50 +1,76 @@
 import React from 'react';
 import Tweet from './Tweet';
+import { v4 } from 'uuid';
+
 
 const tweetList = [
   {
     name: 'Paige Williams',
     username: 'p__williams',
     body: 'Arent business students the worst.',
-    profilePic: `https://avatars3.githubusercontent.com/u/26071756?s=460&v=4`
+    profilePic: `https://avatars3.githubusercontent.com/u/26071756?s=460&v=4`,
+    likes: 3,
+    comments: 0,
+    id: v4()
   },
   {
     name: 'Lil\' Kim',
     username: 'LilKim',
     body: 'Congrats to Cardi B! Love you boo',
-    profilePic: "https://pbs.twimg.com/profile_images/1084603052101906432/qOAqofr2_400x400.jpg"
+    profilePic: "https://pbs.twimg.com/profile_images/1084603052101906432/qOAqofr2_400x400.jpg",
+    likes: 4024,
+    comments: 254,
+    id: v4()
   },
   {
     name: 'Jenna Wortham',
     username: 'jennydeluxe',
     body: 'i really really need to know what face serums are giving BTS that Swarovski shine',
-    profilePic: "https://pbs.twimg.com/profile_images/1059188052239757313/6SoYL4jY_400x400.jpg"
+    profilePic: "https://pbs.twimg.com/profile_images/1059188052239757313/6SoYL4jY_400x400.jpg",
+    likes: 423,
+    comments: 21,
+    id: v4()
   }
 ];
 
-function TweetList(props){
-  const tweetListStyles = {
+const styles = {
+  tweetList: {
     display: 'flex',
     backgroundColor: '#E6ECF0',
     width: '40%',
     flexDirection: 'column'
-  };
+  }
+}
 
 
+class TweetList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      isLiked: false,
+      likedTweet: null
+    };
+  }
 
-  return(
-    <div style={tweetListStyles}>
 
-      {tweetList.map((tweet, index) =>
-        <Tweet name = {tweet.name}
-          username = {tweet.username}
-          body = {tweet.body}
-          profilePic = {tweet.profilePic}
-          key = {index}/>
-      )}
+render(){
+  
+    return(
+      <div style={styles.tweetList}>
 
-    </div>
-  );
+        {tweetList.map((tweet) =>
+          <Tweet name = {tweet.name}
+            username = {tweet.username}
+            body = {tweet.body}
+            profilePic = {tweet.profilePic}
+            likes = {tweet.likes}
+            comments = {tweet.comments}
+            key = {tweet.id}/>
+        )}
+
+      </div>
+    );
+  }
 }
 
 export default TweetList;
